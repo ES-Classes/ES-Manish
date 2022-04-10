c3a804b3ff97879a9bd67371a871b57d5aa7e10c
Manish-hash <manishtimalsina51@gmail.com>
2022-04-10 10:47:00 +0545

    lesson 4

---
+++ Lesson4-Operators.js
@@ -0,0 +1,19 @@
+let x=90;
+let y=90;
+let x1=x++;
+let y1=++y;
+console.log(x,y,x1,y1);
+//Unary operator: ++,--,!
+let z=!x;
+//find the number is even or odd
+let isEven= x%2==0;//13%2=1
+isEven = x& 1==0;
+//x=8
+//1000 & 0001 =0000 =0
+//5= 101 <<1
+//1010
+let p= x<<1;
+let q=y>>1;
+let r=~isEven;
+//comparision
+let s=x== '90';
