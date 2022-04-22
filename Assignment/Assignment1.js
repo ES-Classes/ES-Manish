bc3375cb56537c8603331ef42e4340cbd33a081f
Manish Timalsina <manishtimalsina51@gmail.com>
2022-04-22 22:26:29 +0545

    first assignment

---
+++ Assignment/Assignment1.js
@@ -0,0 +1,12 @@
+let Pmass=92;
+let Rmass=47;
+
+let Pheight=1.70;
+let Rheight=1.90;
+let PBMgoit I=Pmass/(Pheight*Pheight);
+let RBMI=Rmass/(Rmass*Rmass);
+
+let PHigherBMI= PBMI>RBMI;
+console.log(PBMI);
+console.log(RBMI);
+console.log(PHigherBMI);
