21275b2c05f1edca56718b69fcefdad394d9374d
Manish Timalsina <manishtimalsina51@gmail.com>
2022-04-22 22:47:12 +0545

    fourth

---
+++ Class/lesson6-Functions.js
@@ -0,0 +1,33 @@
+printHello = function (name)
+{
+    console.log(`Hello! ${name}`);
+}
+
+function formatName(firstName, lastName)
+{
+    return `Hello! ${lastName}, ${firstName}`;
+}
+
+// Arrow funtions
+let x = (name) => `Hello! ${name}`;
+formatName = (firstName, lastName) => `Hello! ${lastName}, ${firstName}`;
+
+
+printHello('Manish');
+let fullName = formatName('Manish', 'Timalsina');
+console.log(fullName);
+
+
+
+
+function factorial(n) 
+{ 
+    if(n < 2)
+        return 1;
+    
+    return n * factorial(n-1);
+}
+
+let fact = (n) => n < 2 ? 1 : n * fact(n-1);
+
+console.log(fact(5));
