877dc065ba696cdc9317d9c97fcf3c66530e03be
Manish Timalsina <manishtimalsina51@gmail.com>
2022-04-23 12:19:44 +0545

    modified

--- Assignment/Assignment3.js
+++ Assignment/Assignment3.js
@@ -1,6 +1,6 @@
 
-
-function CalculateTip(n){
+/*
+function CalculateTip(n){   
     if(n>50 && n<300){
         return 0.1 * n;
     }
@@ -8,6 +8,24 @@ function CalculateTip(n){
         return 0.15 *n;
     }
 }
+let n=400;
+tip=CalculateTip(n);
+total= n+tip;
+console.log(`The bill was ${n} ,the tip was ${tip} , and the total is Rs ${total}`);
+*/
+
+//using arrow function
+let CalculateTip =(n) => 
+{
+    
+if(n>50 && n<300){
+    return 0.1 * n;
+}
+else{
+    return 0.15 *n;
+}
+}
+
 let n=400;
 tip=CalculateTip(n);
 total= n+tip;
